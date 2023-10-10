b3492c83b38393af5cc59fd3cf9797622e706042
Opilane TTHK <opilane@tthk.ee>
2023-10-10 09:01:49 +0300

    Add files

---
+++ libs/conf.php
@@ -0,0 +1,7 @@
+<?php
+$serverinimi = "localhost";
+$kasutajanimi = "an";
+$parool = "qwerty";
+$andmebaas = "an";
+$yhendus = new mysqli($serverinimi, $kasutajanimi, $parool, $andmebaas);
+$yhendus->set_charset('UTF8');
